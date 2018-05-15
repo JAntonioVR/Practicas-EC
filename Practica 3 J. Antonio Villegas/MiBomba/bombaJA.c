bdd7a4d17946085f9cc394a3d3335561c920c758
JuanAntonio <juanantoniovr98@gmail.com>
2018-05-15 16:28:24 +0200

    Añado las Practicas de EC

---
+++ Practica 3 J. Antonio Villegas/MiBomba/bombaJA.c
@@ -0,0 +1,61 @@
+#include <stdio.h>	// para printf()
+#include <stdlib.h>	// para exit()
+#include <string.h>	// para strncmp()/strlen()
+#include <sys/time.h>	// para gettimeofday(), struct timeval
+
+char password[]="bombafacil\n";
+int  passcode  = 2564;
+
+void boom(){
+	printf("***************\n");
+	printf("*** BOOM!!! ***\n");
+	printf("***************\n");
+	exit(-1);
+}
+
+void defused(){
+	printf("·························\n");
+	printf("··· bomba desactivada ···\n");
+	printf("·························\n");
+	exit(0);
+}
+
+int main(){
+#define SIZE 100
+	char pass[SIZE];
+	int  pasv;
+#define TLIM 7
+	struct timeval tv1,tv2;	// gettimeofday() secs-usecs
+
+	gettimeofday(&tv1,NULL);
+
+	printf("Introduce la contraseña: ");
+	fgets(pass,SIZE,stdin);
+
+	//Comparación innecesaria
+	strncmp(pass,password,strlen(password));
+	//Autentica comparación
+	if (strncmp(pass,password,strlen(password)))
+	    boom();
+	//Otra comparación innecesaria
+	strncmp(pass,password,strlen(password));
+
+	gettimeofday(&tv2,NULL);
+	if (tv2.tv_sec - tv1.tv_sec > TLIM)
+	    boom();
+
+	printf("Introduce el código: ");
+	scanf("%i",&pasv);
+
+	//Modificación del código introducido
+	pasv*=2;
+	pasv-=47;
+	pasv++;
+	if (pasv!=passcode) boom();
+
+	gettimeofday(&tv1,NULL);
+	if (tv1.tv_sec - tv2.tv_sec > TLIM)
+	    boom();
+
+	defused();
+}
